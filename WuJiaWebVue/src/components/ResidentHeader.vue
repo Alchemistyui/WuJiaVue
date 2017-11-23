9b1f3b26c7bfc9661b4302c75ab39409cc68a55b
Alchemist <1319525622@qq.com>
2017-11-23 22:10:05 +0800

    调整css样式，但仍有适配问题

--- WuJiaWebVue/src/components/ResidentHeader.vue
+++ WuJiaWebVue/src/components/ResidentHeader.vue
@@ -1,31 +1,36 @@
 <template>
 	<div id="ResidentHeader" class="box">
+
+		<a href=""><img v-bind:src="logoURL" alt="吾家" id="logo"></a>
 		
 		<div v-for="(list,index) in navLists" class="nav" :class="{ selected:selectedIndex == index}" @click="isSelected(index)"><a href="#">{{list.text}}</a></div>
+
+		<a href=""><img v-bind:src="portraitURL" alt="头像" id="portrait"></a>
+		<a href=""><img v-bind:src="questionURL" alt="问题" id="question"></a>
+		
 
 	</div>
 </template>
 
 <script>
+// 未进行屏幕适配，只是单纯像素显示
 
 export default {
 	data () {
 		return {
 			navLists:[
-		{
-			"text":"首页"                     
-		},
-		{
-			"text":"组件"                     
-		},
-		{
-			"text":"API"                        
-		},
-		{
-			"text":"我们"                     
-		}
-		],
-		selectedIndex: 0
+			{ "text":"首页" },
+			{ "text":"客服人员" },
+			{ "text":"小区管理" },
+			{ "text":"车位管理" },
+			{ "text":"物业管理" },
+			{ "text":"物业账户" },
+			],
+			selectedIndex: 0,
+			logoURL: './src/assets/images/logo.JPG',
+			questionURL: './src/assets/images/question.png',
+			portraitURL: './src/assets/images/portrait.png'
+			
 		}
 	},
 	methods:{
@@ -41,19 +46,47 @@ export default {
 <style type="text/css">
 
 .box{
-	height: 40px;
+	height: 50px;
 	background: rgba(255, 255, 255, 0.5);
+	display: flex;
 }
 .nav{
-	line-height: 40px;
+	line-height: 50px;
 	display: inline-block;
-	margin-left: 100px;
+	margin-left: 50px;
 	cursor: pointer;
 }
 
+a {
+	display: block;
+	color: #2c3e50;
+	text-decoration: none;
+	width: 100px;
+	padding: 0px;
+	font-family: 'Avenir', Helvetica, Arial, sans-serif;
+	font-size: 120%;
+}
 
+#logo {
+	width: 50px;
+	
+}
+
+#question {
+	width: 50px;
+	float: right;
+
+	
+
+}
+#portrait {
+	width: 50px;
+	float: right;
+
+
+}
 
 .selected {
-	background-color: yellow;
+	background-color: #F7DA32;
 }
 </style>
