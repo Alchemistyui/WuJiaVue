c927bd53018602f98e178d0a4270a589474ba1db
Alchemist <1319525622@qq.com>
2018-03-12 19:24:01 +0800

    add element components

--- WuJiaWebVue/src/main.js
+++ WuJiaWebVue/src/main.js
@@ -2,12 +2,18 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+import Element from 'element-ui'
 
 Vue.config.productionTip = false
+Vue.use(ElementUI)
+Vue.use(Element, { size: 'small' })
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   template: '<App/>',
-  components: { App }
+  components: { App },
+  render: h => h(App)
 })
